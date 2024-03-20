c39506d5f3794eaf566d679a9ee895e53c5b16af
ramosarthur <neycopaneycopa@gmail.com>
2024-03-20 10:04:15 -0300

    formulario

--- projeto inicial/scripts.js
+++ projeto inicial/scripts.js
@@ -1,13 +1,33 @@
 var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar, false);
 var nome = document.getElementById("nome");
- 
+var email= document.getElementById("email");
+var saida= document.getElementById("saida-de-dados");
+var telefone= document.getElementById("telefone");
+var cep= document.getElementById("cep");
+var logradouro= document.getElementById("logradouro");
+var numero= document.getElementById("numero");
+
+
 function alertar(event){
-    alert("Você clicou no botão!!! " + nome.value);
+   // alert("Você clicou no botão!!! " + nome.value);
  
-    var numero = 7;
-    var resultado = numero % 2;
-    if(resultado == 0){
-        alert("este número é par!");
-    }
+   // var numero = 7;
+   // var resultado = numero % 2;
+   // if(resultado == 0){
+ //       alert("este número é par!");
+//
+   // }
+    saida.innerText = "nome: "  + nome.value +
+    "\n Email: " + email.value +
+    "\n telefone " + telefone.value +
+    "\n CEP:     " + cep.value + 
+    "\n logradouro: " + logradouro.value +
+    "\n número: " + numero.value;
+
+
+
+
+
+
 }
